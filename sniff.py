df6ff726c45a4de49afba9a157c649a1546dd65d
Matti Ranta <matti@mdranta.net>
2014-04-23 17:34:42 -0400

    * add filter and check to see pif IP exists in packet

--- sniff.py
+++ sniff.py
@@ -18,27 +18,28 @@ myNetMidi = NetMidi(int(options.mport))
 
 # everytime scapy sees a packet this func will be called
 def callback(pkt):
-	if pkt.haslayer(TCP) or pkt.haslayer(UDP): # tcp and udp are only ones that will have port numbers
-		size = pkt[IP].len
-		if TCP in pkt:
-			proto = "TCP"
-			port = min([pkt[TCP].sport,pkt[TCP].dport])
-			eph_port = max([pkt[TCP].sport,pkt[TCP].dport])
-		elif UDP in pkt:
-			proto = "UDP"
-			port = min([pkt[UDP].sport,pkt[UDP].dport])
-			eph_port = max([pkt[UDP].sport,pkt[UDP].dport])
-		myNetMidi.playNote(proto, port, eph_port, size)
-	elif pkt.haslayer(ICMP):
-		# ICMP packets are special
-		size = pkt[IP].len
-		port = pkt[ICMP].type
-		eph_port = (pkt[ICMP].type + 1) * 15
-		myNetMidi.playNote("ICMP", port, eph_port, size)
+	if IP in pkt:
+		if pkt.haslayer(TCP) or pkt.haslayer(UDP): # tcp and udp are only ones that will have port numbers
+			size = pkt[IP].len
+			if TCP in pkt:
+				proto = "TCP"
+				port = min([pkt[TCP].sport,pkt[TCP].dport])
+				eph_port = max([pkt[TCP].sport,pkt[TCP].dport])
+			elif UDP in pkt:
+				proto = "UDP"
+				port = min([pkt[UDP].sport,pkt[UDP].dport])
+				eph_port = max([pkt[UDP].sport,pkt[UDP].dport])
+			myNetMidi.playNote(proto, port, eph_port, size)
+		elif pkt.haslayer(ICMP):
+			# ICMP packets are special
+			size = pkt[IP].len
+			port = pkt[ICMP].type
+			eph_port = (pkt[ICMP].type + 1) * 15
+			myNetMidi.playNote("ICMP", port, eph_port, size)
 
 # start sniffing
 if options.pcap == False:
-        sniff(prn=callback, store=0, iface=options.interface) # listen to interface
+        sniff(prn=callback, filter="tcp or udp or icmp", store=0, iface=options.interface) # listen to interface
 else:
-        sniff(prn=callback, store=0, offline=options.pcap) # read pcap file
+        sniff(prn=callback, filter="tcp or udp or icmp", store=0, offline=options.pcap) # read pcap file
 
