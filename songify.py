02af118c10908931276530caf6cdfe1a878227d3
Matti Ranta <matti@mdranta.net>
2014-04-24 23:08:11 -0400

    * token bucket is now in a different file and being imported

--- songify.py
+++ songify.py
@@ -4,7 +4,7 @@
 from optparse import OptionParser
 from scapy.all import *
 from netmidi import NetMidi
-from time import time
+from tokenbucket import TokenBucket
 
 # get params from command line
 parser = OptionParser()
@@ -49,38 +49,3 @@ else:
         sniff(prn=callback, filter="tcp or udp or icmp", store=0, offline=options.pcap) # read pcap file
 
 
-# leaky bucket algorithm taken from http://code.activestate.com/recipes/511490-implementation-of-the-token-bucket-algorithm/
-class TokenBucket(object):
-    """An implementation of the token bucket algorithm.
-    
-    >>> bucket = TokenBucket(80, 0.5)
-    >>> print bucket.consume(10)
-    True
-    >>> print bucket.consume(90)
-    False
-    """
-    def __init__(self, tokens, fill_rate):
-        """tokens is the total tokens in the bucket. fill_rate is the
-        rate in tokens/second that the bucket will be refilled."""
-        self.capacity = float(tokens)
-        self._tokens = float(tokens)
-        self.fill_rate = float(fill_rate)
-        self.timestamp = time()
-
-    def consume(self, tokens):
-        """Consume tokens from the bucket. Returns True if there were
-        sufficient tokens otherwise False."""
-        if tokens <= self.tokens:
-            self._tokens -= tokens
-        else:
-            return False
-        return True
-
-    def get_tokens(self):
-        if self._tokens < self.capacity:
-            now = time()
-            delta = self.fill_rate * (now - self.timestamp)
-            self._tokens = min(self.capacity, self._tokens + delta)
-            self.timestamp = now
-        return self._tokens
-    tokens = property(get_tokens)
